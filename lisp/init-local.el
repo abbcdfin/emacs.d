c7bbe503e74b4a655ce72f1c84c2ec328bea402d
jian zhang <zhangjian@sys-in.com.cn>
2023-06-18 07:00:24 +1000

    update: a few local customisation.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,10 @@
 (load-theme 'wheatgrass)
 
+
+(set-face-attribute 'default nil :height 121)
+(setq org-format-latex-options '(:foreground default :background default :scale 1.5 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+                                             ("begin" "$1" "$" "$$" "\\(" "\\[")))
+
 (setq c-default-style "linux")
 
 (setq-default c-basic-offset 8
@@ -13,9 +18,11 @@
   (mapcar 'kill-buffer (buffer-list))
   (delete-other-windows))
 
-(setq org-directory "~/workspace/org/org-normal")
-(setq org-default-notes-file "~/workspace/org/org-normal/refile/refile.org")
-(setq org-agenda-files (directory-files-recursively "~/workspace/org/org-normal" "\\.org$"))
+(setq org-directory "~/org/org-normal")
+(setq org-default-notes-file "~/org/org-normal/refile/refile.org")
+;;(setq org-agenda-files '("~/org/org-normal/study"))
+;;(setq org-agenda-file-regexp "*.org")
+(setq org-agenda-files (directory-files-recursively "~/org/org-normal" "\\.org$"))
 
 (setq org-enable-priority-commands t)
 (setq org-default-priority ?E)
@@ -41,14 +48,14 @@
 (setq org-roam-v2-ack t)
 
 ;;(make-directory "~/workspace/org-roam")
-(setq org-roam-directory (file-truename "~/workspace/org/org-roam"))
+(setq org-roam-directory (file-truename "~/org/org-roam"))
 (org-roam-db-autosync-mode)
 
 (autoload 'helm-bibtex "helm-bibtex" "" t)
 
 (require 'org-ref)
-(setq reftex-default-bibliography '("~/workspace/org/bib/jian.lib.bib"))
-(setq bibtex-completion-bibliography '("~/workspace/org/bib/jian.lib.bib"))
+(setq reftex-default-bibliography '("~/org/bib/jian.lib.bib" "~/org/bib/jian.calibre.bib"))
+(setq bibtex-completion-bibliography '("~/org/bib/jian.lib.bib" "~/org/bib/jian.calibre.bib"))
 
 (org-roam-mode)
 (org-roam-bibtex-mode)
@@ -70,8 +77,18 @@
 
 (require 'org-roam-bibtex)
 
-(setq org-archive-location "~/workspace/org/org-normal/archives/2022.org::* From %s")
+(setq org-archive-location "~/org/archives/2023.org::* From %s")
+
+;; yasnippet setup
+(require 'yasnippet)
+(yas/global-mode 1)
 
 (provide 'init-local)
 
 (setq org-image-actual-width nil)
+
+;; latex/auctex
+(defun compile-latex ()
+  "complile latex of the current buffer"
+  (setq web-mode-markup-indent-offset 4))
+(setq TeX-parse-self t)
